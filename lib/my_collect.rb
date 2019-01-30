2c0988f620ac562328af8db713fcd195dd1ab658
Rufus <ruufman@gmail.com>
2019-01-30 22:06:25 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,32 @@
+#def my_collect(collection)
 
+def my_each(array)
+   i = 0
+   while i < array.length
+      yield array[i]
+      i+=1
+   end
+   array
+end
 
+def my_collect(array)
+   i = 0
+   new = []
+   while i < array.length
+      new << yield(array[i])
+      i += 1
+   end
+   new
+end
+
+def my_select(collection)
+   i = 0
+   new = []
+   while i < collection.length
+      if yield(collection[i]) == true
+         new << collection[i]
+      end
+      i += 1
+   end
+   new
+end

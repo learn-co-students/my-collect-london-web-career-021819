98f3d66822447888990d9eb2b546e3051b768e3d
vyskoutaris <vyskoutaris@hotmail.com>
2019-02-05 18:17:44 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
+#1st solution
+def my_collect(collect)
+  array = []
+  collect.each do |collection|
+    yield(collection)
+    array << yield(collection)
+  end
+  array
+end
 
+#2nd solution using while
+def my_collect(collection)
+  array = []
+  i = 0
+  while i < collection.length
+  array.push(yield(collection[i]))
+  i += 1
+  end
+  array
+end
 

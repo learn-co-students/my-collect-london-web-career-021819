e021a98483cb254b404759f6b9c2faaf2ab201df
fernando <fernando_lopezc@live.com>
2019-02-13 09:06:10 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+def my_collect(array)
+if block_given?
+  i = 0
+  new_array = []
+  while i<array.length
+    new_array << yield(array[i].capitalize)
+    i+=1
+  end
+else
+  return
+  end
+  new_array
+  end
 
-
+my_collect(array) do
+  |element| element.split(" ").first
+end
